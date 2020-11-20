76374835577c8f478f276e59bb3346533644b19f
Zhiyi-Zhang-Coder <857746629@qq.com>
2020-11-20 16:52:56 +0800

    chapter1 and chapter2

---
+++ Homework_Chapter2/app/src/main/java/com/zzy/Homework_Chapter2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.zzy.Homework_Chapter2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zzy.Homework_Chapter2.search.SearchActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Intent intent = new Intent(MainActivity.this, MyActivity.class);
+//                intent.putExtra("extra", "from MainActivity");
+//                startActivity(intent);
+
+
+//                Intent intent = new Intent("com.example.chapter2.ACTION_START");
+//                startActivity(intent);
+
+
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
---
+++ Homework_Chapter2/app/src/main/java/com/zzy/Homework_Chapter2/search/SearchActivity.java
@@ -0,0 +1,54 @@
+package com.zzy.Homework_Chapter2.search;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.zzy.Homework_Chapter2.R;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchActivity";
+
+    private RecyclerView mRecyclerView;
+    private SearchAdapter mSearchAdapter = new SearchAdapter();
+
+    private SearchLayout mSearchLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mRecyclerView = findViewById(R.id.rv);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mSearchAdapter);
+
+        final List<String> items = new ArrayList<>();
+        for (int i = 1; i < 100; i++) {
+            items.add("第 " + i + " 个小视频");
+        }
+        mSearchAdapter.notifyItems(items);
+
+        mSearchLayout = findViewById(R.id.search);
+        mSearchLayout.setOnSearchTextChangedListener(new SearchLayout.OnSearchTextChangedListener() {
+            @Override
+            public void afterChanged(String text) {
+                Log.i(TAG, "afterChanged: " + text);
+                List<String> filters = new ArrayList<>();
+                for (String item : items) {
+                    if (item.contains(text)) {
+                        filters.add(item);
+                    }
+                }
+                mSearchAdapter.notifyItems(filters);
+            }
+        });
+
+    }
+}
